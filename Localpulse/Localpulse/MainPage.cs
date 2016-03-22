d236dd291b637a4b528f0e27f3a9eda5af5fc177
Timothy Gu <timothygu99@gmail.com>
2016-03-21 20:04:46 -0700

    Add a MainPage

---
+++ Localpulse/Localpulse/MainPage.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace Localpulse
+{
+	public class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			Label label = new Label {
+				Text = "MainPage",
+				VerticalOptions = LayoutOptions.Center,
+				BackgroundColor = Color.Yellow,
+				TextColor = Color.Black,
+			};
+			Content = label;
+		}
+	}
+}

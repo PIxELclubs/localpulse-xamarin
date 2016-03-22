075a3e26db6477250f9c20577a743e8bbb76a070
Timothy Gu <timothygu99@gmail.com>
2016-03-21 19:48:50 -0700

    Getting started

--- Localpulse/Localpulse.WinPhone/Properties/AssemblyInfo.cs
+++ Localpulse/Localpulse.WinPhone/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Localpulse.WinPhone")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Localpulse")]
+[assembly: AssemblyDescription("Crowdsourced Community Improvement")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Localpulse.WinPhone")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
